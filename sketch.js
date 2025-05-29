b9ee8b2264bde20a5cda551e156d2de83ab41b8b
AgustinForte <168159355+AgustinForte@users.noreply.github.com>
2025-05-29 13:45:56 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+let mic;
+
+function preload() {
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  userStartAudio();
+  mic = new p5.AudioIn();
+  mic.start();
+}
+
+function draw() {
+  background(255, 20); // efecto de desvanecimiento
+  
+}
