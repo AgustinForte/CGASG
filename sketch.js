751156a8e8df2139a3766590e9b2739abb4ecc9b
AgustinForte <168159355+AgustinForte@users.noreply.github.com>
2025-06-16 20:24:47 -0300

    sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,11 @@ let colores = [];
 let mic, amp;
 let ultimoColor = null;
 
-let AMP_MIN = 0.03; 
+let AMP_MIN = 0.03;
+let canvasPinceles;
+
+let tiempoUltimoSonido = 0;
+const TIEMPO_LIMITE_SIN_SONIDO = 5000; // 5 segundos
 
 function preload() {
   pincelesBajo.push(loadImage("imagenes/1.png"));
@@ -20,15 +24,18 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(400,800);
+  createCanvas(400, 800);
   background(255);
+
+  canvasPinceles = createGraphics(400, 800);
+  canvasPinceles.clear();
 
   colores = [
     color(255, 0, 0),
     color(0, 0, 255),
     color(255, 255, 0),
     color(0, 255, 0),
-    color(0),
+    color(0), // negro
   ];
 
   userStartAudio();
@@ -36,6 +43,8 @@ function setup() {
   mic.start();
   amp = new p5.Amplitude();
   amp.setInput(mic);
+
+  tiempoUltimoSonido = millis();
 }
 
 function draw() {
@@ -43,8 +52,22 @@ function draw() {
   volumen = constrain(volumen, 0, 0.3);
 
   if (volumen > AMP_MIN) {
-    let escala = map(volumen, AMP_MIN, 0.3, 0.4, 2.0);
+    tiempoUltimoSonido = millis();
+  }
 
+  let tiempoSinSonido = millis() - tiempoUltimoSonido;
+
+  if (tiempoSinSonido > TIEMPO_LIMITE_SIN_SONIDO) {
+    canvasPinceles.noStroke();
+    canvasPinceles.fill(255, 255, 255, 2); 
+    canvasPinceles.rect(0, 0, width, height);
+  }
+  if (tiempoSinSonido >= TIEMPO_LIMITE_SIN_SONIDO * 2) {
+    canvasPinceles.background(255); 
+  }
+
+  if (volumen > AMP_MIN) {
+    let escala = map(volumen, AMP_MIN, 0.3, 0.4, 2.0);
     let x = random(width);
     let y = random(height);
 
@@ -59,12 +82,15 @@ function draw() {
 
     let col = randomColoresBalanceados();
 
-    tint(col);
-    imageMode(CENTER);
-    image(pincel, x, y, pincel.width * escala, pincel.height * escala);
-    noTint();
+    canvasPinceles.tint(col);
+    canvasPinceles.imageMode(CENTER);
+    canvasPinceles.image(pincel, x, y, pincel.width * escala, pincel.height * escala);
+    canvasPinceles.noTint();
   }
+
+  image(canvasPinceles, 0, 0);
 }
+
 function randomColoresBalanceados() {
   let col;
   do {
@@ -75,7 +101,7 @@ function randomColoresBalanceados() {
   if (red(col) === 0 && green(col) === 0 && blue(col) === 0) {
     col.setAlpha(100); 
   } else {
-    col.setAlpha(255); 
+    col.setAlpha(255);
   }
 
   return col;
