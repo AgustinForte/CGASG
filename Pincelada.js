eb70d22903240e3ef639a7530afdf226a91f501f
AgustinForte <168159355+AgustinForte@users.noreply.github.com>
2025-06-26 11:19:30 -0300

    Add files via upload

---
+++ Pincelada.js
@@ -0,0 +1,47 @@
+class Pincelada {
+  static pinceles = [];
+  static colores = [];
+  static ultimoColor = null;
+
+  static cargarPinceles() {
+    for (let i = 1; i <= 17; i++) {
+      let img = loadImage(`imagenes/${i}.png`);
+      this.pinceles.push(img);
+    }
+
+    this.colores = [
+      color(208, 87, 76),
+      color(40, 97, 164),
+      color(193, 174, 80),
+      color(12, 105, 84),
+      color(0),
+    ];
+  }
+
+  constructor() {
+    this.imagen = random(Pincelada.pinceles);
+    this.color = this.elegirColor();
+  }
+
+  elegirColor() {
+    let col;
+    do {
+      col = color(random(Pincelada.colores));
+    } while (
+      Pincelada.ultimoColor &&
+      red(col) === red(Pincelada.ultimoColor) &&
+      green(col) === green(Pincelada.ultimoColor) &&
+      blue(col) === blue(Pincelada.ultimoColor)
+    );
+
+    Pincelada.ultimoColor = col;
+
+    if (red(col) === 0 && green(col) === 0 && blue(col) === 0) {
+      col.setAlpha(100);
+    } else {
+      col.setAlpha(230);
+    }
+
+    return col;
+  }
+}
