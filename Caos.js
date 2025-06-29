344f3326ab3393bb9a79010c8cd1284d1ee940d3
AgustinForte <168159355+AgustinForte@users.noreply.github.com>
2025-06-29 16:47:21 -0300

    Update Caos.js

--- Caos.js
+++ Caos.js
@@ -1,20 +1,15 @@
 class Caos extends Pincelada {
   constructor() {
-    super();
+    super(Pincelada.pincelesCaos);
     this.x = random(width);
     this.y = random(height);
     this.escala = constrain(2.0, 0.4, 1.0);     
-    this.angulo = random(TWO_PI);
   }
 
   dibujar(g) {
-    g.push();
-    g.translate(this.x, this.y);
-    g.rotate(this.angulo);
     g.tint(this.color);
     g.imageMode(CENTER);
-    g.image(this.imagen, 0, 0, this.imagen.width * this.escala, this.imagen.height * this.escala);
+    g.image(this.imagen, this.x, this.y, this.imagen.width * this.escala, this.imagen.height * this.escala);
     g.noTint();
-    g.pop();
   }
 }
