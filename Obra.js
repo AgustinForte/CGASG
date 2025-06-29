4290458dc0418b69df1b74e944e48b0074e7907e
AgustinForte <168159355+AgustinForte@users.noreply.github.com>
2025-06-29 01:09:21 -0300

    Update Obra.js

--- Obra.js
+++ Obra.js
@@ -13,43 +13,64 @@ class Obra {
     this.amp.setInput(this.mic);
 
     this.tiempoUltimoSonido = millis();
-    this.yFila = 0;
+    
+    // listas por tipo
+    this.filas = [];
+    this.ondas = [];
+    this.caos = [];
+
+    // límites por grupo
+    this.maxFilas = 20;
+    this.maxOndas = 15;
+    this.maxCaos = 10;
   }
 
   actualizar() {
-    let vol = this.amp.getLevel();
-    vol = constrain(vol, 0, 0.3);
+  let vol = this.amp.getLevel();
+  vol = constrain(vol, 0, 0.3);
 
-    if (vol > Obra.AMP_MIN) {
-      this.tiempoUltimoSonido = millis();
+  if (vol > Obra.AMP_MIN) {
+    this.tiempoUltimoSonido = millis();
 
-      let trazo;
-
-      if (vol < 0.1) {
-        trazo = new Fila();
-      } else if (vol < 0.2) {
-        trazo = new Onda();
-      } else {
-        trazo = new Caos();
+    if (vol < 0.1) {
+      if (this.filas.length < this.maxFilas) {
+        let trazo = new Fila();
+        trazo.dibujar(this.canvas);
+        this.filas.push(trazo);
       }
-
-      trazo.dibujar(this.canvas);
-    }
-
-    const sinSonido = millis() - this.tiempoUltimoSonido;
-
-    if (sinSonido > Obra.TIEMPO_LIMITE) {
-      this.canvas.noStroke();
-      this.canvas.fill(255, 255, 255, 2);
-      this.canvas.rect(0, 0, width, height);
-    }
-
-    if (sinSonido > Obra.TIEMPO_LIMITE * 2) {
-      this.canvas.background(255);
+    } else if (vol < 0.2) {
+      if (this.ondas.length < this.maxOndas) {
+        let trazo = new Onda();
+        trazo.dibujar(this.canvas);
+        this.ondas.push(trazo);
+      }
+    } else {
+      if (this.caos.length < this.maxCaos) {
+        let trazo = new Caos();
+        trazo.dibujar(this.canvas);
+        this.caos.push(trazo);
+      }
     }
   }
 
-  mostrar() {
+  const sinSonido = millis() - this.tiempoUltimoSonido;
+
+  if (sinSonido > Obra.TIEMPO_LIMITE) {
+    this.canvas.noStroke();
+    this.canvas.fill(255, 255, 255, 2);
+    this.canvas.rect(0, 0, width, height);
+  }
+
+  if (sinSonido > Obra.TIEMPO_LIMITE * 2) {
+    this.canvas.background(255);
+
+    this.filas = [];
+    this.ondas = [];
+    this.caos = [];
+  }
+}
+
+mostrar() {
     image(this.canvas, 0, 0);
   }
 }
