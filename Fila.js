aa35fe5449052bac9fcf98c159a97f00e6f48eb5
AgustinForte <168159355+AgustinForte@users.noreply.github.com>
2025-07-10 09:01:51 -0300

    Update Fila.js

--- Fila.js
+++ Fila.js
@@ -1,20 +1,14 @@
 class Fila extends Pincelada {
-  constructor(volumen) {
+  constructor() {
     super(Pincelada.pincelesFila);
     this.x = random(width); 
     this.y = random(height);
     this.escala = 1.0;
   }
-
-  dibujar(g, vol = 0) {
-    let escalaFinal = this.escala;
-    if (vol > 0) {
-      escalaFinal *= map(vol, Obra.AMP_MIN, 0.3, 1.0, 2.0);
-    }
-
+  dibujar(g) {
     g.tint(this.color);
     g.imageMode(CENTER);
-    g.image(this.imagen, this.x, this.y, this.imagen.width * escalaFinal, this.imagen.height * escalaFinal);
+    g.image(this.imagen, this.x, this.y, this.imagen.width * this.escala, this.imagen.height * this.escala);
     g.noTint();
   }
 }
