4e32c147fb89c0e06148e9256d3cf4e5c3ffc931
Anahiganem <168236893+Anahiganem@users.noreply.github.com>
2025-07-10 15:43:34 -0300

    Onda.js

--- Onda.js
+++ Onda.js
@@ -1,13 +1,13 @@
 class Onda extends Pincelada {
-  constructor() {
+  constructor(x) {
     super(Pincelada.pincelesOnda);
-    this.x = random(width); 
-    this.y = random(height);
+    this.x = x; 
+    this.y = 0;
     this.escala = 1.0;     
   }
   dibujar(g) {
     g.tint(this.color);
-    g.imageMode(CENTER);
+    g.imageMode(CORNER);
     g.image(this.imagen, this.x, this.y, this.imagen.width * this.escala, this.imagen.height * this.escala);
     g.noTint();
   }
