e7ec6f4d450aad9522c7c1537ee07dbbaa5a70ed
AgustinForte <168159355+AgustinForte@users.noreply.github.com>
2025-07-10 12:29:58 -0300

    Add files via upload

---
+++ Cortas.js
@@ -0,0 +1,14 @@
+class Cortas extends Pincelada {
+  constructor() {
+    super(Pincelada.pincelesCortas);
+    this.x = random(width); 
+    this.y = random(height);
+    this.escala = 1.0;     
+  }
+  dibujar(g) {
+    g.tint(this.color);
+    g.imageMode(CENTER);
+    g.image(this.imagen, this.x, this.y, this.imagen.width * this.escala, this.imagen.height * this.escala);
+    g.noTint();
+  }
+}
