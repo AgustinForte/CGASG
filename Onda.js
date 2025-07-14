d5c7220217ba7b205e08c8d007384d1d5c78e033
pilargarayzabal <167646094+pilargarayzabal@users.noreply.github.com>
2025-07-14 16:42:44 -0300

    Update Onda.js

--- Onda.js
+++ Onda.js
@@ -1,14 +1,23 @@
 class Onda extends Pincelada {
   constructor(x) {
     super(Pincelada.pincelesOnda);
-    this.x = x; 
-    this.y = 0;
-    this.escala = 1.0;     
+    this.x = x;
+    this.escala = 1.0;
   }
+
   dibujar(g) {
     g.tint(this.color);
-    g.imageMode(CORNER);
-    g.image(this.imagen, this.x, this.y, this.imagen.width * this.escala, this.imagen.height * this.escala);
+    g.imageMode(CENTER);
+
+    let imgW = this.imagen.width * this.escala;
+    let imgH = this.imagen.height * this.escala;
+
+    
+    for (let y = -imgH / 2; y < g.height + imgH; y += imgH * 0.85) {
+      let offsetX = sin((y + frameCount) * 0.02 + this.x * 0.01) * 20;
+      g.image(this.imagen, this.x + offsetX, y, imgW, imgH);
+    }
+
     g.noTint();
   }
 }
