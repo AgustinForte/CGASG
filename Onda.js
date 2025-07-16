4cb6bdb466ba3654797af484b5c1208a8abecff5
Anahiganem <168236893+Anahiganem@users.noreply.github.com>
2025-07-16 13:01:30 -0300

    Update Onda.js

--- Onda.js
+++ Onda.js
@@ -14,7 +14,7 @@ class Onda extends Pincelada {
 
     
     for (let y = -imgH / 2; y < g.height + imgH; y += imgH * 0.85) {
-      let offsetX = sin((y + frameCount) * 0.02 + this.x * 0.01) * 20;
+      let offsetX = sin((y + this.x) * 0.02 + this.x * 0.01) * 20;
       g.image(this.imagen, this.x + offsetX, y, imgW, imgH);
     }
 
